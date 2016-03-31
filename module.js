dd292dfb7f0523bcf1e3cd88ef6f44d8687cec3c
zhuangtongfa <zhuangtongfa@qq.com>
2016-03-31 23:44:27 +0800

    新增模块化基础框架

---
+++ module.js
@@ -0,0 +1,21 @@
+//模块化基本框架,支持amd,common js ,以及浏览器下直接使用
+(function(global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return factory(global);
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory;
+  } else {
+    global.zz = factory(global);
+  }
+})(this, function(global) {
+  'use strict';
+  function zzz() {
+    console.log('dosomething...');
+    //逻辑代码
+  }
+  return zzz;
+});
+
+// 浏览器下 直接new zzz() or import zzz from './zzz.js' new zzz()
