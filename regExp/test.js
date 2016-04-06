c19aeb2ae1d232133e4818346a3bb553c6027a88
zhuangtongfa <zhuangtongfa@qq.com>
2016-04-06 22:02:01 +0800

    improve

--- regExp/test.js
+++ regExp/test.js
@@ -1,4 +1,3 @@
 var myRegExp=require("./regExp.js")
 console.log(myRegExp.phoneReg.test('13169670619'))
 console.log(myRegExp.userNameReg.test('zhuanhf'))
-console.log(myRegExp.userNameReg.test('哈哈'))
