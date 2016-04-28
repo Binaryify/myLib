c00f5d04fcef64d19b0f4f29617cf58cadcd618c
Binaryify <zhuangtongfa@qq.com>
2016-04-28 13:47:12 +0800

    add emojiRegExp

--- regExp/regExp.js
+++ regExp/regExp.js
@@ -16,6 +16,7 @@
     userNameReg:/^(?!_)(?!.*?_$)[a-zA-Z0-9_\u4e00-\u9fa5]+$/,  //正则表达式，只含有汉字、数字、字母、下划线不能以下划线开头和结尾
     emailReg:/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
     tabReg:/(\/tab[0-9+])/,
+    emojiReg:/\ud83d[\ude00-\ude4f]/g, //http://stackoverflow.com/questions/22006218/replace-emoji-unicode-symbol-using-regexp-in-javascript
     test:/(\w+)(.+)\1/ ,  //反向引用 ,匹配相同字符中间夹住的内容
     test2:/<(\w+)>(.+)<\/\1>/g  , //反向引用,匹配标签,
     test3:/s/
---
+++ unicode.js
@@ -0,0 +1,13 @@
+/*
+*js Unicode编码转换
+*/
+var decToHex = function(str) {
+    var res=[];
+    for(var i=0;i < str.length;i++)
+        res[i]=("00"+str.charCodeAt(i).toString(16)).slice(-4);
+    return "\\u"+res.join("\\u");
+}
+var hexToDec = function(str) {
+    str=str.replace(/\\/g,"%");
+    return unescape(str);
+}
